7d9aa8993ee76e30a6ec4bd9b04d6c6a7c19ebff
Stephan Robotta <stephan.robotta@bfh.ch>
2024-01-18 17:36:32 +0100

    update existing files.itemid to questionnaire_response_file.id

--- version.php
+++ version.php
@@ -25,10 +25,18 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+<<<<<<< HEAD
 $plugin->version = 2024082900;  // The current module version (Date: YYYYMMDDXX).
+=======
+$plugin->version = 2023101501;  // The current module version (Date: YYYYMMDDXX).
+>>>>>>> 1df20f1 (update existing files.itemid to questionnaire_response_file.id)
 $plugin->requires = 2022112800.00; // Moodle version (4.1.0).
 
 $plugin->component = 'mod_questionnaire';
 
+<<<<<<< HEAD
 $plugin->release = '4.1.1 (Build - 2024082900)';
+=======
+$plugin->release = '4.1.1 (Build - 2023101501)';
+>>>>>>> 1df20f1 (update existing files.itemid to questionnaire_response_file.id)
 $plugin->maturity = MATURITY_STABLE;
