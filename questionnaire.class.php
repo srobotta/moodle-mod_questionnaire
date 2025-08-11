9a673c4b5cd44eb21e42016668a482bf47e158fe
Stephan Robotta <stephan.robotta@bfh.ch>
2025-08-11 10:31:58 +0200

    Remove change introduces by us.

--- questionnaire.class.php
+++ questionnaire.class.php
@@ -875,7 +875,6 @@ class questionnaire {
      *
      * @param int|bool $userid
      * @param int $groupid
-     * @param int|bool $includeincomplete
      * @return array
      */
     public function get_responses($userid = false, $groupid = 0, $includeincomplete = false) {
@@ -890,12 +889,6 @@ class questionnaire {
             $params['groupid'] = $groupid;
         }
 
-        $statuscnd = '';
-        if (!$includeincomplete) {
-            $statuscnd = ' AND r.complete = :status ';
-            $params['status'] = 'y';
-        }
-
         // Since submission can be across questionnaires in the case of public questionnaires, need to check the realm.
         // Public questionnaires can have responses to multiple questionnaire instances.
         if ($this->survey_is_public_master()) {
@@ -904,13 +897,14 @@ class questionnaire {
                 'INNER JOIN {questionnaire} q ON r.questionnaireid = q.id ' .
                 'INNER JOIN {questionnaire_survey} s ON q.sid = s.id ' .
                 $groupsql .
-                'WHERE s.id = :surveyid' . $statuscnd . $groupcnd;
+                'WHERE s.id = :surveyid AND r.complete = :status' . $groupcnd;
             $params['surveyid'] = $this->sid;
+            $params['status'] = 'y';
         } else {
             $sql = 'SELECT r.* ' .
                 'FROM {questionnaire_response} r ' .
                 $groupsql .
-                'WHERE r.questionnaireid = :questionnaireid' . $statuscnd . $groupcnd;
+                'WHERE r.questionnaireid = :questionnaireid' . $groupcnd;
             $params['questionnaireid'] = $this->id;
         }
         if ($userid) {
